421847c97c83ca26a010f012d1fda595e1cfa0b0
Martinsen <kmar7070@740s-pl-111.local>
2016-09-08 08:40:49 -0600

    Added print my name as private string

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -2,9 +2,15 @@ package stupid.controller;
 
 public class StupidController 
 {
+	private String myName;
+	public StupidController()
+	{
+		myName = "Kaden Martinsen";
+	}
 
 	public void start()
 	{
 		System.out.println("this program is less stupid");
+		System.out.println("My name is " +myName);
 	}
 }
