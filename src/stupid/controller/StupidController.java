2eba954f72ef86e7087b5b3e80b1803dd3b5ac6d
Martinsen <kmar7070@740s-pl-111.local>
2016-09-08 09:30:30 -0600

    added input from keyboard to ask how your day is

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,16 +1,29 @@
 package stupid.controller;
 
-public class StupidController 
+import java.util.Scanner;
+
+public class StupidController
 {
 	private String myName;
+	private Scanner inputReader;
+
 	public StupidController()
 	{
 		myName = "Kaden Martinsen";
+		inputReader = new Scanner(System.in);
 	}
 
 	public void start()
 	{
 		System.out.println("this program is less stupid");
-		System.out.println("My name is " +myName);
+		System.out.println("My name is " + myName);
+		askQuestions();
+	}
+	
+	private void askQuestions()
+	{
+		System.out.println("How is your day?");
+		String input = inputReader.nextLine();
+		System.out.println("Oh you said: " + input + "about your day");
 	}
 }
