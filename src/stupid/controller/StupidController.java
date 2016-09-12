870601bf511b7946ce2b26aa5ac0ea90b300c206
kmart22 <kmar7070@740s-pl-111.local>
2016-09-12 08:35:38 -0600

    created second question favorite food

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -25,5 +25,18 @@ public class StupidController
 		System.out.println("How is your day?");
 		String input = inputReader.nextLine();
 		System.out.println("Oh you said: " + input + " about your day");
+		
+		System.out.println("My next question is this: what type of food is the best?");
+		String foodInput = inputReader.next();
+		System.out.println ("OK, you typed this: " + foodInput);
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
